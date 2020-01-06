93e7ede0596e0edf729002ae829d7e4329a6f2c6
softprops <d.tangren@gmail.com>
2020-01-05 20:40:14 -0500

    correct lang

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function run() {
     }
 
     const matchersPath = path.join(__dirname, '..', '.github');
-    console.log(`##[add-matcher]${path.join(matchersPath, 'go.json')}`);
+    console.log(`##[add-matcher]${path.join(matchersPath, 'rust.json')}`);
 
   } catch (error) {
     core.setFailed(error.message);
